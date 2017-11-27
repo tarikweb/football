18d6f68c58d2ec31fd9a0d7b301414f1fd0e89d9
tarikweb <tarik.abbas94@gmail.com>
2017-11-28 00:07:34 +0100

    modification du joueur (en cours)

---
+++ visualisation/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class ModifierJoueurForm(forms.Form):
+	id_joueur = 
+    nom = forms.CharField(max_length=100)
+    prenom = forms.CharField(max_length=100)
+    age = 
+    taille = 
+    poste = 
+    status = 
+    disponible = 
+    position =
