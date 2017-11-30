c465a7a9b4f32febfada8ac7752eed6b6ee57b24
root <root@debian>
2017-11-30 13:00:19 +0100

    modification de la bdd chacun doit la faire

--- documents_annexes/football.sql
+++ documents_annexes/football.sql
@@ -27,29 +27,6 @@ use football;
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `Coach`
---
-
-CREATE TABLE IF NOT EXISTS `Coach` (
-  `Id_Entraineur` int(11) NOT NULL,
-  `Id_Equipe` int(11) NOT NULL,
-  `Id_Joueur` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `django_migrations`
---
-
-CREATE TABLE IF NOT EXISTS `django_migrations` (
-`id` int(11) NOT NULL,
-  `app` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `applied` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 -- --------------------------------------------------------
 
 --
@@ -57,9 +34,10 @@ CREATE TABLE IF NOT EXISTS `django_migrations` (
 --
 
 CREATE TABLE IF NOT EXISTS `Entraineur` (
-  `Id_Entraineur` int(11) NOT NULL,
+`Id_Entraineur` int(11) NOT NULL,
   `Nom` varchar(15) DEFAULT NULL,
-  `Prenom` varchar(10) DEFAULT NULL
+  `Prenom` varchar(10) DEFAULT NULL,
+  `Id_Equipe` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -69,19 +47,28 @@ CREATE TABLE IF NOT EXISTS `Entraineur` (
 --
 
 CREATE TABLE IF NOT EXISTS `Equipe` (
-  `Id_Equipe` int(11) NOT NULL,
+`Id_Equipe` int(11) NOT NULL,
   `Nom` varchar(10) DEFAULT NULL,
   `Nb_joueurs` varchar(10) DEFAULT NULL,
   `Systeme_jeu` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Equipe`
 --
 
 INSERT INTO `Equipe` (`Id_Equipe`, `Nom`, `Nb_joueurs`, `Systeme_jeu`) VALUES
-(0, 'Sportive T', '25', NULL),
-(1, 'CA Metz', '25', NULL);
+(1, 'CA Metz', '25', NULL),
+(2, 'ES Fameck', '25', NULL),
+(3, 'UL Rombas', '25', NULL),
+(4, 'ES Metz', '25', NULL),
+(5, 'FC Yutz', '25', NULL),
+(6, 'AS Vagney', '25', NULL),
+(7, 'COVillers', '25', NULL),
+(8, 'FC Toul', '25', NULL),
+(9, 'Epinal', '25', NULL),
+(10, 'US Forbach', '25', NULL),
+(13, 'CA Boulay', '25', NULL);
 
 -- --------------------------------------------------------
 
@@ -90,7 +77,7 @@ INSERT INTO `Equipe` (`Id_Equipe`, `Nom`, `Nb_joueurs`, `Systeme_jeu`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `Joueur` (
-  `Id_Joueur` int(11) NOT NULL,
+`Id_Joueur` int(11) NOT NULL,
   `Nom` varchar(15) DEFAULT NULL,
   `Prenom` varchar(25) DEFAULT NULL,
   `Age` int(11) DEFAULT NULL,
@@ -99,38 +86,34 @@ CREATE TABLE IF NOT EXISTS `Joueur` (
   `Status` int(11) DEFAULT NULL,
   `Disponible` tinyint(1) DEFAULT NULL,
   `Position` int(11) DEFAULT NULL,
+  `Numero` int(11) NOT NULL,
   `Id_Equipe` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Joueur`
 --
 
-INSERT INTO `Joueur` (`Id_Joueur`, `Nom`, `Prenom`, `Age`, `Taille`, `Poste`, `Status`, `Disponible`, `Position`, `Id_Equipe`) VALUES
-(0, 'Benjamin', 'Lopez', 25, 166, 'MD', 3, 1, 3, 1),
-(1, 'Bradley', 'Richards', 31, 178, 'DD', 1, 0, 3, 1),
-(2, 'Castro', 'Daniel', 23, 156, 'DD', 1, 0, 5, 1),
-(3, 'Ingram', 'Branch', 24, 181, 'DC', 2, 1, 11, 1),
-(4, 'Rocha', 'Fowler', 26, 200, 'DG', 3, 1, 3, 1),
-(5, 'Dickson', 'Foley', 23, 180, 'MD', 3, 0, 2, 1),
-(6, 'Guy', 'Keller', 29, 214, 'AD', 3, 1, 4, 1),
-(7, 'Diaz', 'Oneill', 38, 194, 'DC', 2, 0, 6, 1),
-(8, 'Guerra', 'Goodwin', 38, 189, 'AD', 2, 1, 3, 1),
-(9, 'Sweeney', 'Schroeder', 39, 169, 'MD', 1, 0, 8, 1),
-(10, 'Whitaker', 'Benton', 26, 171, 'MG', 1, 0, 5, 1),
-(11, 'Palmer', 'Sargent', 40, 177, 'DA', 3, 1, 4, 1),
-(12, 'Alvarez', 'Albert', 33, 177, 'MD', 3, 0, 8, 1),
-(13, 'Henry', 'Wilkinson', 35, 185, 'MG', 3, 1, 9, 1),
-(14, 'Valencia', 'Houston', 24, 210, 'DM', 3, 0, 11, 1),
-(15, 'Odonnell', 'Summers', 32, 200, 'AV', 3, 0, 10, 1),
-(16, 'Harding', 'Cain', 26, 187, 'DG', 1, 1, 3, 1),
-(17, 'Goff', 'Ellis', 37, 205, 'MD', 2, 1, 1, 1),
-(18, 'Wagner', 'Gates', 35, 191, 'DM', 2, 0, 2, 1),
-(19, 'Marks', 'Schwartz', 34, 178, 'DC', 1, 0, 9, 1),
-(20, 'Tanner', 'Nguyen', 40, 171, 'MD', 1, 1, 8, 1),
-(21, 'Parks', 'Salazar', 35, 160, 'AD', 3, 1, 3, 1),
-(22, 'Richards', 'Estrada', 29, 169, 'AG', 3, 0, 11, 1),
-(23, 'Mendez', 'Pruitt', 26, 207, 'DM', 2, 1, 2, 1);
+INSERT INTO `Joueur` (`Id_Joueur`, `Nom`, `Prenom`, `Age`, `Taille`, `Poste`, `Status`, `Disponible`, `Position`, `Numero`, `Id_Equipe`) VALUES
+(1, 'Bradley', 'Richards', 31, 178, 'DD', 1, 0, 3, 0, 1),
+(2, 'Castro', 'Daniel', 23, 156, 'DD', 1, 0, 5, 0, 1),
+(3, 'Ingram', 'Branch', 24, 181, 'DC', 2, 1, 11, 0, 1),
+(4, 'Rocha', 'Fowler', 26, 200, 'DG', 3, 1, 3, 0, 1),
+(5, 'Dickson', 'Foley', 23, 180, 'MD', 3, 0, 2, 0, 1),
+(6, 'Guy', 'Keller', 29, 214, 'AD', 3, 1, 4, 0, 1),
+(7, 'Diaz', 'Oneill', 38, 194, 'DC', 2, 0, 6, 0, 1),
+(8, 'Guerra', 'Goodwin', 38, 189, 'AD', 2, 1, 3, 0, 1),
+(9, 'Sweeney', 'Schroeder', 39, 169, 'MD', 1, 0, 8, 0, 1),
+(10, 'Whitaker', 'Benton', 26, 171, 'MG', 1, 0, 5, 0, 1),
+(11, 'Palmer', 'Sargent', 40, 177, 'DA', 3, 1, 4, 0, 1),
+(12, 'Alvarez', 'Albert', 33, 177, 'MD', 3, 0, 8, 0, 1),
+(13, 'Henry', 'Wilkinson', 35, 185, 'MG', 3, 1, 9, 0, 1),
+(14, 'Valencia', 'Houston', 24, 210, 'DM', 3, 0, 11, 0, 1),
+(15, 'Odonnell', 'Summers', 32, 200, 'AV', 3, 0, 10, 0, 1),
+(16, 'Harding', 'Cain', 26, 187, 'DG', 1, 1, 3, 0, 1),
+(17, 'Goff', 'Ellis', 37, 205, 'MD', 2, 1, 1, 0, 1),
+(18, 'Wagner', 'Gates', 35, 191, 'DM', 2, 0, 2, 0, 1),
+(19, 'Marks', 'Schwartz', 34, 178, 'DC', 1, 0, 9, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -139,7 +122,7 @@ INSERT INTO `Joueur` (`Id_Joueur`, `Nom`, `Prenom`, `Age`, `Taille`, `Poste`, `S
 --
 
 CREATE TABLE IF NOT EXISTS `Statistique` (
-  `Id_stat` int(11) NOT NULL,
+`Id_stat` int(11) NOT NULL,
   `Temps_jeu` int(11) DEFAULT NULL,
   `But` int(11) DEFAULT NULL,
   `Passe_decisive` int(11) DEFAULT NULL,
@@ -150,12 +133,6 @@ CREATE TABLE IF NOT EXISTS `Statistique` (
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `Coach`
---
-ALTER TABLE `Coach`
- ADD PRIMARY KEY (`Id_Entraineur`,`Id_Equipe`,`Id_Joueur`), ADD KEY `FK_Coach_Id_Equipe` (`Id_Equipe`), ADD KEY `FK_Coach_Id_Joueur` (`Id_Joueur`);
 
 --
 -- Indexes for table `django_migrations`
@@ -197,16 +174,28 @@ ALTER TABLE `Statistique`
 ALTER TABLE `django_migrations`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `Entraineur`
+--
+ALTER TABLE `Entraineur`
+MODIFY `Id_Entraineur` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Equipe`
+--
+ALTER TABLE `Equipe`
+MODIFY `Id_Equipe` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `Joueur`
+--
+ALTER TABLE `Joueur`
+MODIFY `Id_Joueur` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `Statistique`
+--
+ALTER TABLE `Statistique`
+MODIFY `Id_stat` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `Coach`
---
-ALTER TABLE `Coach`
-ADD CONSTRAINT `FK_Coach_Id_Entraineur` FOREIGN KEY (`Id_Entraineur`) REFERENCES `Entraineur` (`Id_Entraineur`),
-ADD CONSTRAINT `FK_Coach_Id_Equipe` FOREIGN KEY (`Id_Equipe`) REFERENCES `Equipe` (`Id_Equipe`),
-ADD CONSTRAINT `FK_Coach_Id_Joueur` FOREIGN KEY (`Id_Joueur`) REFERENCES `Joueur` (`Id_Joueur`);
 
 --
 -- Constraints for table `Joueur`
--- visualisation/models.py
+++ visualisation/models.py
@@ -6,21 +6,12 @@ from django.forms import ModelForm
 
 
 # Create your models here.
-class Coach(models.Model):
-    id_entraineur = models.ForeignKey('Entraineur', models.DO_NOTHING, db_column='Id_Entraineur', primary_key=True)  # Field name made lowercase.
-    id_equipe = models.ForeignKey('Equipe', models.DO_NOTHING, db_column='Id_Equipe')  # Field name made lowercase.
-    id_joueur = models.ForeignKey('Joueur', models.DO_NOTHING, db_column='Id_Joueur')  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'Coach'
-        unique_together = (('id_entraineur', 'id_equipe', 'id_joueur'),)
-
 
 class Entraineur(models.Model):
-    id_entraineur = models.IntegerField(db_column='Id_Entraineur', primary_key=True)  # Field name made lowercase.
+    id_entraineur = models.AutoField(db_column='Id_Entraineur', primary_key=True)  # Field name made lowercase.
     nom = models.CharField(db_column='Nom', max_length=15, blank=True, null=True)  # Field name made lowercase.
     prenom = models.CharField(db_column='Prenom', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    id_equipe = models.IntegerField(db_column='Id_Equipe')  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -28,7 +19,7 @@ class Entraineur(models.Model):
 
 
 class Equipe(models.Model):
-    id_equipe = models.IntegerField(db_column='Id_Equipe', primary_key=True)  # Field name made lowercase.
+    id_equipe = models.AutoField(db_column='Id_Equipe', primary_key=True)  # Field name made lowercase.
     nom = models.CharField(db_column='Nom', max_length=10, blank=True, null=True)  # Field name made lowercase.
     nb_joueurs = models.CharField(db_column='Nb_joueurs', max_length=10, blank=True, null=True)  # Field name made lowercase.
     systeme_jeu = models.CharField(db_column='Systeme_jeu', max_length=10, blank=True, null=True)  # Field name made lowercase.
@@ -39,7 +30,7 @@ class Equipe(models.Model):
 
 
 class Joueur(models.Model):
-    id_joueur = models.IntegerField(db_column='Id_Joueur', primary_key=True)  # Field name made lowercase.
+    id_joueur = models.AutoField(db_column='Id_Joueur', primary_key=True)  # Field name made lowercase.
     nom = models.CharField(db_column='Nom', max_length=15, blank=True, null=True)  # Field name made lowercase.
     prenom = models.CharField(db_column='Prenom', max_length=25, blank=True, null=True)  # Field name made lowercase.
     age = models.IntegerField(db_column='Age', blank=True, null=True)  # Field name made lowercase.
@@ -48,6 +39,7 @@ class Joueur(models.Model):
     status = models.IntegerField(db_column='Status', blank=True, null=True)  # Field name made lowercase.
     disponible = models.IntegerField(db_column='Disponible', blank=True, null=True)  # Field name made lowercase.
     position = models.IntegerField(db_column='Position', blank=True, null=True)  # Field name made lowercase.
+    numero = models.IntegerField(db_column='Numero')  # Field name made lowercase.
     id_equipe = models.ForeignKey(Equipe, models.DO_NOTHING, db_column='Id_Equipe', blank=True, null=True)  # Field name made lowercase.
 
     class Meta:
@@ -56,7 +48,7 @@ class Joueur(models.Model):
 
 
 class Statistique(models.Model):
-    id_stat = models.IntegerField(db_column='Id_stat', primary_key=True)  # Field name made lowercase.
+    id_stat = models.AutoField(db_column='Id_stat', primary_key=True)  # Field name made lowercase.
     temps_jeu = models.IntegerField(db_column='Temps_jeu', blank=True, null=True)  # Field name made lowercase.
     but = models.IntegerField(db_column='But', blank=True, null=True)  # Field name made lowercase.
     passe_decisive = models.IntegerField(db_column='Passe_decisive', blank=True, null=True)  # Field name made lowercase.
@@ -66,19 +58,3 @@ class Statistique(models.Model):
     class Meta:
         managed = False
         db_table = 'Statistique'
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class JoueurForm(ModelForm):
-	class Meta:
-		model = Joueur
-		fields = ['id_joueur','nom','prenom','age','taille','poste','status','disponible','position']
