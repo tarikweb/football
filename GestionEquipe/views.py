d9d9782c713e9914a72312b486df486a976e66d6
root <root@debian>
2017-12-08 12:04:04 +0100

    ajout de la page accueil avec url

--- GestionEquipe/views.py
+++ GestionEquipe/views.py
@@ -16,6 +16,7 @@ from .forms import classementForm
 #test
 # Create your views here.
 
+
 def home(request):
     """ Exemple de page HTML, non valide pour que l'exemple soit concis """
     text = """<h1>Bienvenue sur mon blog !</h1>
@@ -58,3 +59,12 @@ def supprimer(request):
 	if request.method== "POST":
 		reponse="ok"
 		return JsonResponse({'reponse' : 'reponse'})
+
+def Accueil(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>Bienvenue sur mon blog !</h1>
+              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
+    
+
+    #return HttpResponse(text)
+    return render(request,'GestionEquipe/Accueil.html',locals())
