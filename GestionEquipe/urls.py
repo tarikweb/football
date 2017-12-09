a33550494315f914c6cde3d0643bc96d1ee08814
root <root@debian>
2017-12-09 21:44:11 +0100

    ok c'est bon

--- GestionEquipe/urls.py
+++ GestionEquipe/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
    # url(r'^visualisation$', views.home),
-    url(r'^$', views.home, name='GestionEquipe'),
+    url(r'^GestionEquipe$', views.home, name='GestionEquipe'),
     url(r'^$', views.Accueil, name='Accueil'),
     url(r'^article/(?P<id_article>\d+)$', views.view_article),
     url(r'^ajax/supprimer/$', views.supprimer, name='supprimer'),
