dbf844812c418597bc8fa0395c2e536e57cbccab
root <root@debian>
2017-12-26 11:38:46 +0100

    creation de la page utilisateur

--- football/settings.py
+++ football/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware', 
 ]
 
 ROOT_URLCONF = 'football.urls'
